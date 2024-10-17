c2074b5baec9eb50c2ee07ae80510c5f60cdedff
EugenioBarbieriViale <eugenio.barbieri.viale@gmail.com>
2024-10-17 22:30:07 +0200

    new

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use chrono::Datelike;
+
 fn main() {
-    println!("Hello, world!");
+    let now = chrono::offset::Local::now();
+    let weekday: String = now.date().weekday().to_string();
+
+    let now: String = now.to_string();
+
+    let date = &now[0..10];
+    let time = &now[11..19];
+
+    println!("{now}");
+    println!("{date}");
+    println!("{time}");
+    println!("{weekday}");
 }
