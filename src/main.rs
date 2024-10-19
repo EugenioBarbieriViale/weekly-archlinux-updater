7804294fdd0e622dc69279d419a9245c6be17f32
EugenioBarbieriViale <eugenio.barbieri.viale@gmail.com>
2024-10-19 19:56:41 +0200

    new

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,49 @@
-use std::fs::File;
+use std::fs::OpenOptions;
 use std::io::prelude::*;
 use chrono::Datelike;
 
+struct Event {
+    date: String,
+    time: String,
+    weekday: String,
+}
+
+impl Event {
+    fn new() -> Self {
+        let now = chrono::offset::Local::now();
+        let weekday: String = now.date_naive().weekday().to_string();
+
+        let now: String = now.to_string();
+
+        let date = now[0..10].to_string();
+        let time = now[11..19].to_string();
+
+        Self {
+            date,
+            time,
+            weekday,
+        }
+    }
+
+    fn event_str(&self) -> String {
+        self.date.clone() + " - " + &self.time + " - " + &self.weekday
+    }
+}
+
 fn main() {
-    let now = chrono::offset::Local::now();
-    let weekday: String = now.date_naive().weekday().to_string();
+    let filename = "history.log";
+    let ev = Event::new();
 
-    let now: String = now.to_string();
+    handle_history(&ev.event_str(), filename);
+}
 
-    let date = &now[0..10];
-    let time = &now[11..19];
+fn handle_history(event: &String, filename: &str) {
+    let mut file = OpenOptions::new()
+        .write(true)
+        .append(true)
+        .create(true)
+        .open(filename)
+        .expect("Can't open file!");
 
-    println!("{now}");
-    println!("{date}");
-    println!("{time}");
-    println!("{weekday}");
-
-    let event = date.to_owned() + " - " + time + " - " + &weekday;
-
-    let mut file = File::create("history.log");
-    file.write_all(b"{event}");
+    writeln!(file, "{}", event).expect("Can't write to file");
 }
